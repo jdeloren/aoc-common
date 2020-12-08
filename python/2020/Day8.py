154b84c5450b4e89ca78275961615a3a7fd27250
Jamison DeLorenzo <aquacrayfish@gmail.com>
2020-12-08 23:49:39 +0000

    Day 8 complete

--- python/2020/Day8.py
+++ python/2020/Day8.py
@@ -2,70 +2,119 @@ import sys
 from common import DataAnalyzer
 
 
-
 class Program:
+    
+    def duplicates(self, value):
+        return value in self._executed
 
-    def __init__(self, instructions, acc_default=0, max_depth=50, debug=False):
+
+    def __init__(self, instructions, acc_default=0, max_depth=5000, debug=False):
+        self._default_accumulator = acc_default
+
         self.instructions = instructions
-        self.accumulator = acc_default
+        self.accumulator = self._default_accumulator
+        self.terminator = self.duplicates
+        self._terminated = False
+        self._depth_exceeeded = False
         self.pointer = 0
         self.depth = max_depth
         self.debug = debug
-
-        self.switcher = {
-            'nop': exec_noop,
-            'acc': exec_acc,
-            'jmp': exec_jmp,
-        }
+        self._executed = []
 
 
-        def __debug(self, value):
-            self.debug = value
+    def __debug(self, value):
+        self.debug = value
 
 
-        def exec_noop(self, value):
-            print("NO-OP {:d}".format(value)) if self.debug else pass
-            pass
+    def execute(self):
+        if self.terminator is not None and self.terminator(self.pointer):
+            print("TERMINATED on duplicate instruction: {:d}, {:d}".format(self.accumulator, self.pointer))
+            self._terminated = True
+            return
 
+        if self.pointer >= len(self.instructions):
+            self._terminated = True
+            print("TERMINATED on pointer indexing: {:d} @ {:d}".format(self.pointer, self.accumulator))
+            return
 
-        def exec_acc(self, value):
-            print("ACC {:d}".format(value)) if self.debug else pass
+        self._executed.append(self.pointer)
+        instruction = self.instructions[self.pointer]
+        # print("PROCESSING {:s}".format(instruction))
+
+        i = instruction.split(' ')
+        command = i[0]
+        value = int(i[1:][0])
+        # print("MOD: {:d} from {:} is {:d} << {:d}".format(mod, i, value, int(i[1:][0])))
+
+        if command == 'nop':
+            print("NO-OP {:d}".format(value)) if self.debug else None
+            self.pointer += 1
+        elif command == 'acc':
             self.accumulator += value
-
-
-        def exec_jmp(self, value):
-            print("JUMP {:d}".format(value)) if self.debug else pass
+            self.pointer += 1
+            print("ACC {:d} => {:d} @ {:d}".format(value, self.accumulator, self.pointer)) if self.debug else None
+        elif command == 'jmp':
             self.pointer += value
+            print("JUMP {:d} => {:d}".format(value, self.pointer)) if self.debug else None
+            self.execute()
+        else:
+            print("INVALID INSTRUCTION: {:s} {:}".format(command, value))
+            self.pointer = -1
 
 
-        def instruction(self, command, value):
-            return switcher.get(key)(value)
+    def reset(self, instructions=[]):
+        self.pointer = 0
+        self.accumulator = self._default_accumulator
+        self._executed.clear()
+        self._terminated = False
+        self._depth_exceeeded = False
+
+        if instructions:
+            self.instructions = instructions
+
+
+    def run(self):
+        self.reset()
+        # print("START: {:} {:}".format(not self._terminated, len(self._executed) < self.depth))
+        while not self._terminated and len(self._executed) < self.depth:
+            self.execute()
+
+        if len(self._executed) >= self.depth:
+            self._depth_exceeeded = True
         
 
-        def run(self):
-            while self.pointer in range(0, len(self.instructions)):
-                code = self.instructions[i]
-
-
-
-def run_program(data):
-    accumulator = 0
-
-    for inst in data:
-        pieces = inst.split(' ')
-        mod = 1 if pieces[1][0] == '+' else -1
-        instruction(pieces[0], mod * pieces[1:], accumulator)
+def modify(instructions, mod):
+    modified = instructions.copy()
+    while 'acc' in modified[mod]:
+        mod += 1
+        
+    if 'nop' in modified[mod]:
+        modified[mod] = modified[mod].replace('nop', 'jmp')
+    else:
+        modified[mod] = modified[mod].replace('jmp', 'nop')
+    
+    return mod+1, modified
 
 
 def second():
-    # print("(8.2) question count: {:d}".format(questions(data, audit_unique)))
+    print("(8.2) program corruption detection algorithm")
+    instructions = DataAnalyzer.text("2020/day8.txt")
+    n, i2 = modify(instructions, 0)
+    p = Program(i2)
+    p.terminator = None
+    p.run()
 
+    if p._depth_exceeeded:
+        while p._depth_exceeeded:
+            n, i2 = modify(instructions, n)
+            p.reset(i2)
+            p.run()
+    
 
 def first():
-    data = DataAnalyzer.text("2020/day8.txt")
+    print("(8.1) program execution")
     p = Program(DataAnalyzer.text("2020/day8.txt"), 0)
     p.run()
-    # print("(8.1) question count: {:d}".format(questions(data)))
 
 
 def solve(puzzle):
