3f0d736cbe6dd0a4ff910a708a986cec334b554a
jdelorenzo <Astacus#300>
2021-12-01 09:12:53 -0500

    2021 Day 1

---
+++ js/2020/Day5.js
@@ -0,0 +1,65 @@
+const {DataAnalyzer} = require('../common/DataAnalyzer.js')
+let data = DataAnalyzer.strs("2020/day5.txt")
+
+
+function checker(pass, lo, hi, start=0,end=127) {
+    pass.split("").forEach(id => {
+        if (id === lo) {
+            end -= Math.floor((end-start+1)/2)
+        } else {
+            start += Math.floor((end-start+1)/2)
+        }
+    })
+
+    return pass[pass.length-1] === hi ? end : start
+}
+
+function max(passes) {
+    return Math.max(...passes)
+}
+
+function unique(passes) {
+    let sorted = passes.sort((a, b) => a-b)
+    let current = sorted[0]-1
+
+    for (let i = 0; i < sorted.length; current = sorted[i], ++i) {
+        if (sorted[i] !== current + 1) {
+            return sorted[i]-1
+        }
+    }
+
+    return -1
+}
+
+function calc(tabulator=max) {
+    let passes = []
+    data.forEach(pass => {
+        row = checker(pass.slice(0, -3), 'F', 'B', 0, 127)
+        col = checker(pass.slice(-3), 'L', 'R', 0, 7)
+        passes.push((row*8)+col)
+    })
+
+    return tabulator(passes)
+}
+
+function one() {
+    console.log("(2020.5): " + calc(max))
+}
+
+function two() {
+    console.log("(2020.5): " + calc(unique))
+}
+
+switch(process.argv.slice(2)[0]) {
+    case '1':
+        one()
+        break
+
+    case '2':
+        two()
+        break
+    
+    default:
+        one()
+        two()
+}
---
+++ js/2021/Day1.js
@@ -0,0 +1,46 @@
+const {DataAnalyzer} = require('../common/DataAnalyzer.js')
+let data = DataAnalyzer.ints("2021/day1.txt")
+
+function basic(numbers=data) {
+    let count = 0
+    for (let i = 1; i <= numbers.length; ++i) {
+        if (numbers[i-1] < numbers[i]) {
+            count++
+        }
+    }
+    return count
+}
+
+function builder() {
+    let numbers = []
+    for (let i = 0; i < data.length-2; ++i) {
+        numbers.push(data[i] + data[i+1] + data[i+2])
+    }
+    
+    return basic(numbers)
+}
+
+function one() {
+    answer = basic()
+    console.log("(2021.1): " + answer)
+}
+
+function two() {
+    answer = builder()
+    console.log("(2020.2): " + answer)
+}
+
+
+switch(process.argv.slice(2)[0]) {
+    case '1':
+        one()
+        break
+
+    case '2':
+        two()
+        break
+    
+    default:
+        one()
+        two()
+}
