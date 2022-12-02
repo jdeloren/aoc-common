738ce530af4ec20b73bb77cd207d21987e5108e1
Jamison DeLorenzo <Jamison.DeLorenzo@gradientai.com>
2022-12-02 08:45:05 -0500

    Day 2

---
+++ python/2022/Day2.py
@@ -0,0 +1,59 @@
+import sys
+from common import DataAnalyzer
+
+input = DataAnalyzer.text("2022/day2.txt")
+
+def play(data, splashback=False):
+
+    def hose_soaker(rounds):
+        p1 = {'A' : 'R', 'B': 'P', 'C': 'S'}
+        p2 = {'X' : 'R', 'Y': 'P', 'Z': 'S'}
+        
+        play = {'R' : 1, 'P': 2, 'S': 3}
+        winners = ['RP', 'PS', 'SR']
+
+        score = 0
+
+        def spray(cpu, player):
+            if player == 'Y':
+                return cpu
+            elif player == 'X':
+                return {'R': 'S', 'P': 'R', 'S': 'P'}[cpu]
+
+            return {'R': 'P', 'P': 'S', 'S': 'R'}[cpu]
+
+        for i in rounds:
+            a = p1[i[0]]
+            b = p2[i[1]] if not splashback else spray(a, i[1])
+
+            if a == b:
+                score += play[b] + 3
+            else:
+                score += (play[b]) + (6 if a+b in winners else 0)
+
+        return score
+
+    rounds = [d.split(' ') for d in data]
+    
+    return hose_soaker(rounds)  # enough with the hose!
+
+
+def second():
+    print(f"(2022 2.2) nothing beats that! => {play(input, splashback=True)}")
+
+def first():
+    print(f"(2022 2.1) good ol' rock => {play(input)}")
+
+
+def solve(puzzle):
+    if puzzle == '1':
+        first()
+    elif puzzle == '2':
+        second()
+    else:
+        first()
+        second()
+
+
+if __name__ == '__main__':
+    solve(sys.argv[1])
