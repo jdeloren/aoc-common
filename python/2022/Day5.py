d3fd52ec9b225cbb8b2d312f0cd8402ad89ff5a5
Jamison DeLorenzo <Jamison.DeLorenzo@gradientai.com>
2022-12-05 09:12:26 -0500

    Day 5

---
+++ python/2022/Day5.py
@@ -0,0 +1,53 @@
+import sys
+from common import DataAnalyzer, Solver
+
+input = DataAnalyzer.text("2022/day5.txt")
+
+def pop(list, count):
+    items = list[:count]
+    del list[:count]
+    return items
+
+def arrange(data, reverse=True):
+    instructions = False
+    crates = []
+    moves = []
+
+    def tops(crates):
+        toppers = ''
+        for crate in crates:
+            toppers += crate[0]
+        return toppers
+
+    # build crates
+    for i in range(int(len(data[0]) / 4)+1):
+        crates.append([])    
+
+    for row in data:
+        if not instructions:
+            if len(row) == 0:
+                instructions = True
+            elif not row.startswith(' 1'):
+                for i in range(0, int(len(row)/4)+1):
+                    crates[i] += row[i*4+1] if row[i*4+1] != ' ' else []
+        else:
+            moves.append(row)
+    
+    # start moves
+    for move in moves:
+        steps = move.split(' ')
+        boxes = pop(crates[int(steps[3])-1], int(steps[1]))
+        if reverse:
+            boxes.reverse()
+        crates[int(steps[5])-1][:0] = boxes
+    
+    return tops(crates)
+
+def second():
+    print(f"(2022 5.2) top crates CM9001 => {arrange(input, False)}")
+
+def first():
+    print(f"(2022 5.1) top crates CM9000 => {arrange(input)}")
+
+if __name__ == '__main__':
+    Solver.solve(sys.argv[1], first, second)
