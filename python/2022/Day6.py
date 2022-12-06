b8163e5b646401a54ce4355aac18f999569f9800
Jamison DeLorenzo <Jamison.DeLorenzo@gradientai.com>
2022-12-06 07:43:59 -0500

    Cleaner output

--- python/2022/Day6.py
+++ python/2022/Day6.py
@@ -5,9 +5,9 @@ input = DataAnalyzer.text("2022/day6.txt")[0]
 
 def marker(signal, length=4):
     for i in range(0, len(signal)-length):
-        str = set(signal[i:i+length])
-        if len(str) == length:
-            return i+length, str
+        check = set(signal[i:i+length])
+        if len(check) == length:
+            return i+length, ''.join(check)
     
     return ''
 
