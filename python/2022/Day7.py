ca65a3c4f0f0b8e7bda90ee8ad0e835981097c5a
Jamison DeLorenzo <Jamison.DeLorenzo@gradientai.com>
2022-12-07 14:16:21 -0500

    Day 7

---
+++ python/2022/Day7.py
@@ -0,0 +1,143 @@
+import sys
+from common import DataAnalyzer, Solver
+
+input = DataAnalyzer.text("2022/day7.txt")
+
+
+class Filesystem:
+    def __init__(self):
+        self._root = Node('/')
+        self._cwd = self._root
+        self._registry_size = 0
+        self._partition_size = 70000000
+        self._update_partition_size = 30000000
+    
+    def dir(self):
+        return self._cwd
+    
+    def mkdir(self, name):
+        self._cwd._dirs.append(Node(name, self._cwd))
+    
+    def touch(self, name, size):
+        file = Node(name, self._cwd)
+        file.__size__ = size
+        self._cwd._files.append(file)
+    
+    def cd(self, name):
+        if name == '/':
+            self._cwd = self._root
+        elif name == '..':
+            self._cwd = self._cwd.parent        
+        else:
+            for dir in self._cwd._dirs:
+                if dir.name == name:
+                    self._cwd = dir
+                    break
+    
+    def tree(self):
+        self._root.stat()
+
+    def syscheck(self, limit=100000):
+        size = 0
+        self._registry_size = 0
+
+        for dir in self._root.nodes():
+            size += dir.size(limit)
+
+        return size
+    
+    def du(self):
+        return [dir.du() for dir in self._root._dirs]
+    
+    def update(self):
+        if self._partition_size - len(self._root) < self._update_partition_size:
+            diff = self._update_partition_size - (self._partition_size - len(self._root))
+            dirs = [item for subs in self.du() for item in subs]
+            from operator import itemgetter
+            dirs.sort(key=itemgetter(1))
+
+            for dir in dirs:
+                if dir[1] >= diff:
+                    return dir
+        
+        return ('', '-1')
+    
+    def __len__(self):
+        return len(self._root)
+
+
+class Node:
+    def __init__(self, name='', parent=None):
+        self.name = name
+        self.parent = parent
+
+        self._dirs = []
+        self._files = []
+        self._depth = 0 if parent == None else parent._depth + 1
+        self.__size__ = 0
+    
+    def __len__(self):
+        return self.__size__ if self.file() else sum([len(i) for i in self.nodes()])
+    
+    def nodes(self):
+        return self._dirs + self._files
+    
+    def file(self):
+        return not len(self._dirs) > 0 and not len(self._files) > 0
+    
+    def du(self):
+        x = [(self.name, len(self))]
+        [x.extend(dir.du()) for dir in self._dirs]
+        return x
+
+    def stat(self):
+        typeinfo = ' (' + (f'dir size={len(self)}' if not self.file() else 'file, size=' + str(len(self))) + ')'
+        print('  '*self._depth + ' - ' + self.name + typeinfo)
+        for node in self.nodes():
+            node.stat()
+    
+    def size(self, limit=99999999):
+        size = len(self)
+        size = 0 if self.file() or size > limit else size
+
+        for d in self._dirs:
+            d.size(limit)
+        
+        filesystem._registry_size += size
+        return size
+
+filesystem = None
+
+
+def registry_check():
+    filesystem.syscheck()
+    return filesystem._registry_size
+
+def update():
+    return filesystem.update()
+
+def run(input, cmd):
+    global filesystem
+    filesystem = Filesystem()
+
+    for i in input:
+        if i.startswith('$ '):
+            if 'cd ' in i:
+                filesystem.cd(i.split(' ')[2])
+        else:
+            if i.startswith('dir '):
+                filesystem.mkdir(i.split(' ')[1])
+            else:
+                split = i.split(' ')
+                filesystem.touch(split[1], int(split[0]))
+
+    return cmd()
+
+def second():
+    print(f"(2022 7.2) dir to delete for update => {run(input, update)}")
+
+def first():
+    print(f"(2022 7.1) total dir size => {run(input, registry_check)}")
+
+if __name__ == '__main__':
+    Solver.solve(sys.argv[1], first, second)
