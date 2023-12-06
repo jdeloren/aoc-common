a8fc86eb64437ee50aefd53e88cc0c367b7a12f6
Jamison DeLorenzo <Jamison.DeLorenzo@gradientai.com>
2023-12-06 09:06:49 -0500

    Day 6

---
+++ python/2023/Day6.py
@@ -0,0 +1,31 @@
+import sys, math
+from typing import List
+from common import DataAnalyzer, Solver
+
+input = DataAnalyzer.text("2023/day6.txt")
+
+test_data = [
+'Time:      7  15   30',
+'Distance:  9  40  200'
+]
+
+
+def race(t: int, d: int):
+    return sum(map(lambda x: 1 if x * (t - x) > d else 0, range(t)))
+
+def parse(data: List[str]) -> int:
+    return math.prod(map(race, [int(t) for t in data[0].split(':')[1].split()], [int(d) for d in data[1].split(':')[1].split()]))
+
+def long_parse(data: List[str]) -> int:
+    return race(int(''.join(data[0].split(':')[1].split())), int(''.join(data[1].split(':')[1].split())))
+
+def second():
+    print(f"(2023 5.2) score => {long_parse(test_data)}")
+    print(f"(2023 5.2) score => {long_parse(input)}")
+
+def first():
+    print(f"(2023 5.1) score => {parse(test_data)}")
+    print(f"(2023 5.1) score => {parse(input)}")
+
+if __name__ == '__main__':
+    Solver.solve(sys.argv[1], first, second)
